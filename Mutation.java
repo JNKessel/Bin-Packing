63ff5876d52e3be41359123b5b7dda5ca4425a36
costalonga <33266172+costalonga@users.noreply.github.com>
2018-05-01 16:07:13 -0300

    Add files via upload

--- Mutation.java
+++ Mutation.java
@@ -6,15 +6,15 @@ import java.util.ArrayList;
 public class Mutation {
 
 	public static ArrayList<Integer> Realocate(ArrayList<Integer> pai) {
-		
+
 		int tam = pai.size();
 		int r1, r2, aux;
 		ArrayList<Integer> res = (ArrayList<Integer>) pai.clone();
 
-		//Gera dois numeros distintos, aleatoriamente, menores que o tamanho do pai
+		// Gera dois numeros distintos, aleatoriamente, menores que o tamanho do pai
 		do {
-			r1 = ThreadLocalRandom.current().nextInt(0, tam + 1);
-			r2 = ThreadLocalRandom.current().nextInt(0, tam + 1);
+			r1 = ThreadLocalRandom.current().nextInt(0, tam);
+			r2 = ThreadLocalRandom.current().nextInt(0, tam);
 		} while (r1 == r2);
 
 		// Garante que r1 será sempre o menor numero
@@ -23,15 +23,15 @@ public class Mutation {
 			r1 = r2;
 			r2 = aux;
 		}
-		
-		//Substitui o valor que estava na posição r2 até a posicção 
-		//r1+1 e desloca os outros que estavam entre r1 e r2 para direita
-		for(int i=0; (r2-i) > r1; i++) {
-			aux = res.get(r2-i);
-			res.set(r2-i, res.get(r2-(i+1)));
-			res.set(r2-(i+1), aux);
+
+		// Substitui o valor que estava na posição r2 até a posicção
+		// r1+1 e desloca os outros que estavam entre r1 e r2 para direita
+		for (int i = 0; (r2 - i) > r1; i++) {
+			aux = res.get(r2 - i);
+			res.set(r2 - i, res.get(r2 - (i + 1)));
+			res.set(r2 - (i + 1), aux);
 		}
-		
+
 		return res;
 	}
 
@@ -41,17 +41,50 @@ public class Mutation {
 		int r1, r2, aux;
 		ArrayList<Integer> res = (ArrayList<Integer>) pai.clone();
 
-		//Gera dois numeros distintos, aleatoriamente, menores que o tamanho do pai
+		// Gera dois numeros distintos, aleatoriamente, menores que o tamanho do pai
 		do {
-			r1 = ThreadLocalRandom.current().nextInt(0, tam + 1);
-			r2 = ThreadLocalRandom.current().nextInt(0, tam + 1);
+			r1 = ThreadLocalRandom.current().nextInt(0, tam);
+			r2 = ThreadLocalRandom.current().nextInt(0, tam);
 		} while (r1 == r2);
 
-		//Substitui os valores 
+		// Substitui os valores
 		aux = res.get(r1);
 		res.set(r1, res.get(r2));
 		res.set(r2, aux);
 
+		return res;
+	}
+
+	public static ArrayList<Integer> Two_Opt(ArrayList<Integer> pai) {
+
+		int tam = pai.size();
+		int r1, r2, aux;
+		ArrayList<Integer> res = (ArrayList<Integer>) pai.clone();
+
+		// Gera dois numeros distintos, aleatoriamente, menores que o tamanho do pai
+		do {
+			r1 = ThreadLocalRandom.current().nextInt(0, tam);
+			r2 = ThreadLocalRandom.current().nextInt(0, tam);
+		} while (r1 == r2);
+
+		// Garante que r1 será sempre o menor numero
+		if (r1 < r2) {
+			aux = r1;
+			r1 = r2;
+			r2 = aux;
+		}
+		
+		int n = 25;
+				
+		for(int i=0; i < n; i++) {
+			int ri1 = ThreadLocalRandom.current().nextInt(r1, r2+1);
+			int ri2 = ThreadLocalRandom.current().nextInt(r1, r2+1);
+			
+			aux = res.get(ri1);
+			res.set(ri1, res.get(ri2));
+			res.set(ri2, aux);
+		}
+		
 		return res;
 	}
 
